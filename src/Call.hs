440e8e9f5377dee944861e04898f1131835ceedb
xiaoyu <xiaoyu@gini.co>
2022-06-24 19:56:37 +0800

    Add CallOptions

---
+++ src/Call.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Call(CallOption) where 
+
+import qualified Data.Time as T
+
+import Data.Aeson hiding (json)
+import Language.Haskell.TH
+import Data.Aeson.TH
+import Data.Aeson.Types
+
+data CallOption = PoolBalance Float
+                | BondBalance Float
+                | PoolFactor Float
+                | BondFactor Float
+                | OnDate T.Day
+                | AfterDate T.Day
+                | And CallOption CallOption 
+                | Or CallOption CallOption 
+                deriving (Show)
+
+$(deriveJSON defaultOptions ''CallOption)
