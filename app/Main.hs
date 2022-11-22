c6bc82d3c3b6a25746e27b96e587da87441e6148
xiaoyu <xiaoyu@simplicity.vip>
2022-11-22 14:25:49 +0800

    bump version

--- app/Main.hs
+++ app/Main.hs
@@ -84,7 +84,7 @@ getVersionR :: Handler String
 getVersionR =  do
   addHeader "Access-Control-Allow-Origin" "*"
   addHeader "Access-Control-Allow-Methods" "GET"
-  return "{\"version\":\"0.4.0\"}"
+  return "{\"version\":\"0.4.0.1\"}"
 
 optionsVersionR :: Handler String 
 optionsVersionR = do
