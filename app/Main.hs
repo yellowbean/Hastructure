0eaaf3d2662f87ccbcf5f6bba1bc95258cc1ac18
xiaoyu <always.zhang@gmail.com>
2023-02-24 02:41:23 +0800

    bump version to-> < 0.8.7 >

--- app/Main.hs
+++ app/Main.hs
@@ -128,7 +128,7 @@ postRunDealR = do
 
 getVersionR :: Handler String
 getVersionR =  let 
-                 _v = "0.8.6"
+                 _v = "0.8.7"
                in
                  do
                    addHeader "Access-Control-Allow-Origin" "*"
