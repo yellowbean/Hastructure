a9c71560c828cdfd3dc0d3e281effba54741405f
Xiaoyu <always.zhang@gmail.com>
2023-06-09 14:30:10 +0800

    bump version to-> < 0.18.1 >

--- app/Main.hs
+++ app/Main.hs
@@ -84,7 +84,7 @@ $(deriveJSON defaultOptions ''Version)
 instance ToSchema Version
 
 version1 :: Version 
-version1 = Version "0.18.0"
+version1 = Version "0.18.1"
 
 data PoolType = MPool (P.Pool AB.Mortgage)
               | LPool (P.Pool AB.Loan)
