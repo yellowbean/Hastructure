b6ed66397847516149b1578f9d9608a3c459408d
Xiaoyu <always.zhang@gmail.com>
2023-08-18 00:42:36 +0800

    include SONIA/IRPH

--- src/Types.hs
+++ src/Types.hs
@@ -109,6 +109,7 @@ data Index = LPR5Y
             | EURIBOR6M
             | EURIBOR12M
             | IRPH --  The IRPH (Índice de Referencia de Préstamos Hipotecarios) is a reference index used in Spain to fix the interest rate of mortgage loans
+            | SONIA 
             deriving (Show,Eq,Generic)
 
 type Floater = (Index,Spread)
