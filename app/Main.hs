44b10664a8ce92d6ecc55109ff1fcfdb3ac3e5a9
Xiaoyu <always.zhang@gmail.com>
2024-02-27 02:47:22 +0800

    update version

--- app/Main.hs
+++ app/Main.hs
@@ -93,7 +93,7 @@ $(deriveJSON defaultOptions ''Version)
 instance ToSchema Version
 
 version1 :: Version 
-version1 = Version "0.25.0"
+version1 = Version "0.26.0"
 
 data PoolType = MPool (P.Pool AB.Mortgage)
               | LPool (P.Pool AB.Loan)
