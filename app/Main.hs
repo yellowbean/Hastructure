0154d80f6c9ad77635c2203f61d1675c32420b64
Xiaoyu <always.zhang@gmail.com>
2024-04-26 03:19:38 +0800

    bump version to-> < 0.27.10 >

--- app/Main.hs
+++ app/Main.hs
@@ -102,7 +102,7 @@ $(deriveJSON defaultOptions ''Version)
 instance ToSchema Version
 
 version1 :: Version 
-version1 = Version "0.27.9"
+version1 = Version "0.27.10"
 
 
 data DealType = MDeal (DB.TestDeal AB.Mortgage)
