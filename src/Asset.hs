d93a6e78d3a2499661209cee8b6e7b301cbb2f89
Xiaoyu <always.zhang@gmail.com>
2025-02-13 20:29:15 +0800

    extend to stressByTs

--- src/Asset.hs
+++ src/Asset.hs
@@ -235,7 +235,7 @@ buildDefaultRates a ds mDa =
         do
           rs <- buildDefaultRates a ds (Just x)
           let r = getTsVals $ multiplyTs Inc (zipTs (tail ds) rs) ts 
-          return r
+          return $ capWith 1.0 r
 
       Just (A.DefaultByTerm rs) -> 
         let 
