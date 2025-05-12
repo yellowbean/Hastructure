1c5cd3084aad9755e1b3269d67634c03bae99e75
Xiaoyu <always.zhang@gmail.com>
2025-05-13 00:15:40 +0800

    bump version to-> < 0.45.5 >

--- app/Main.hs
+++ app/Main.hs
@@ -100,7 +100,7 @@ debug = flip Debug.Trace.trace
 
 
 version1 :: Version 
-version1 = Version "0.45.4"
+version1 = Version "0.45.5"
 
 
 wrapRun :: DealType -> Maybe AP.ApplyAssumptionType -> AP.NonPerfAssumption -> RunResp
