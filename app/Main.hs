88d4e1880bc52007dc1e17a2978bf7e8edf27aa9
yellowbean <always.zhang@gmail.com>
2026-02-27 08:05:08 +0800

    bump version to-> < 0.52.3 >

--- app/Main.hs
+++ app/Main.hs
@@ -100,7 +100,7 @@ debug = flip Debug.Trace.trace
 
 
 version1 :: Version 
-version1 = Version "0.52.2"
+version1 = Version "0.52.3"
 
 
 wrapRun :: [D.ExpectReturn] -> DealType -> Maybe AP.ApplyAssumptionType -> AP.NonPerfAssumption -> RunResp
